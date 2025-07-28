4ae744a26788d74ffb62b21487ccdbd71201f98d
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-07-29 00:08:36 +0800

    Finish index draft

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,59 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
+import { useRouter } from "expo-router";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
+  const router = useRouter();
+
   return (
-    <View className="items-center py-72">
+    <LinearGradient
+      colors={["#d9d9d9", "#737373"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+      style={{ flex: 1 }}
+    >
+      <View className="items-center py-72">
+      {/* Title */}
       <Text className="text-5xl text-[#1e1e1e] pb-2">
         <Text className="font-opensans">Train</Text>
         <Text className="font-opensans-bold">In</Text>
       </Text>
 
-      <Text className="font-opensans text-xl">Automated Attendance System</Text>
+      {/* Subtitle */}
+      <Text className="font-opensans text-xl mb-8">
+        Automated Attendance System
+      </Text>
+
+      {/* Time In Button */}
+      <Pressable
+        onPress={() => router.push("/tabs/scan-time-in")}
+        className="border border-[#1e1e1e] rounded-[25px] items-center justify-center m-5 mt-96"
+        style={{
+          width: 150,
+          height: 50,
+          backgroundColor: "transparent",
+        }}
+      >
+        <Text className="font-opensans text-[#1e1e1e] text-xl">
+          Time In
+        </Text>
+      </Pressable>
+
+      {/* Time Out Button */}
+      <Pressable
+        onPress={() => router.push("/tabs/scan-time-out")}
+        className="border border-[#1e1e1e] rounded-[25px] items-center justify-center"
+        style={{
+          width: 150,
+          height: 50,
+          backgroundColor: "transparent",
+        }}
+      >
+        <Text className="font-opensans text-[#1e1e1e] text-xl">
+          Time Out
+        </Text>
+      </Pressable>
     </View>
+    </LinearGradient>
   );
 }
