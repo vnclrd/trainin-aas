7107b24eab2cffcb0b0a5ff444a4c76c4d7e790d
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-07-29 13:07:15 +0800

    Finish scan-time-out draft

--- app/(tabs)/scan-time-out.tsx
+++ app/(tabs)/scan-time-out.tsx
@@ -1,12 +1,26 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import { Text, View, Image, Pressable } from "react-native";
+import { useRouter } from "expo-router";
+import { LinearGradient } from "expo-linear-gradient";
 
-const scanTimeOut = () => {
+export default function ScanTimeIn() {
+  const router = useRouter();
+
   return (
-    <view>
-      <Text>Scan Time Out</Text>
-    </view>
-  )
+    <LinearGradient
+      colors={["#d9d9d9", "#737373"]}
+      start={{ x: 0, y: 0 }}
+      end={{ x: 0, y: 1 }}
+      className="flex-1"
+    >
+      <View className="flex-1 justify-center items-center py-72">
+        <Image
+          source={require("../../assets/images/nfc-wayfinding-mark.png")}
+          className="w-[250px] h-[200px]"
+          resizeMode="contain"
+        />
+        {/* Translate marginTop to Tailwind's mt-{value} class */}
+        <Text className="mt-[30px] text-[30px] color-[#1e1e1e]">Scan to Time Out.</Text>
+      </View>
+    </LinearGradient>
+  );
 }
-
-export default scanTimeOut
