465886aa58758b8e49044bab09f10ad3afc01473
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-07-29 16:51:43 +0800

    Edit scan-time-in and scan-time-out

--- app/(tabs)/scan-time-in.tsx
+++ app/(tabs)/scan-time-in.tsx
@@ -1,59 +1,22 @@
-import { View, Image, Animated } from "react-native";
-import { useRouter } from "expo-router";
+import { View, Text, Image } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
-import React, { useEffect, useRef } from "react";
 
-export default function ScanTimeIn() {
-  const router = useRouter();
-  const fadeAnim = useRef(new Animated.Value(1)).current;
-
-  useEffect(() => {
-    // Fade in and out continuously
-    Animated.loop(
-      Animated.sequence([
-        Animated.timing(fadeAnim, {
-          toValue: 1,
-          duration: 1000,
-          useNativeDriver: true,
-        }),
-        Animated.timing(fadeAnim, {
-          toValue: 0.25,
-          duration: 1000,
-          useNativeDriver: true,
-        }),
-        Animated.timing(fadeAnim, {
-          toValue: 1,
-          duration: 1000,
-          useNativeDriver: true,
-        }),
-      ])
-    ).start();
-  }, [fadeAnim]);
-
-  return (
-    <LinearGradient
-      colors={["#d9d9d9", "#737373"]}
-      start={{ x: 0, y: 0 }}
-      end={{ x: 0, y: 1 }}
-      className="flex-1"
-    >
-      <View className="flex-1 justify-center items-center">
+export default function ScantTimeIn() {
+	return (
+		<LinearGradient
+			colors={[ "#d9d9d9", "#737373" ]}
+			start={{ x: 0, y: 0 }}
+			end={{ x: 0, y: 1 }}
+			className="flex-1"
+		>
+			<View className="flex-1 justify-center items-center">
         <Image
-          source={require("../../assets/images/nfc-wayfinding-mark.png")}
-          className="w-[250px] h-[200px]"
-          resizeMode="contain"
-        />
-        <Animated.Text
-          style={{
-            marginTop: 30,
-            fontSize: 30,
-            color: "#1e1e1e",
-            opacity: fadeAnim, // Bind opacity to animation
-          }}
-        >
-          Scan to Time In.
-        </Animated.Text>
-      </View>
-    </LinearGradient>
-  );
+        source={require('../../assets/images/nfc-wayfinding-mark.png')}
+        className="w-[250px] h-[200px]"
+        resizeMode="contain"
+      />
+				<Text className="text-[30px] color-[#1e1e1e] mt-[30px]">Tap Card to Time In.</Text>
+			</View>
+		</LinearGradient>
+	)
 }
