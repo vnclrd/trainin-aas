f3f2dd01d1282080866d1df1b1bd7a2ace31816a
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-07-29 22:17:28 +0800

    Fix scan-time-out on mobile

--- app/(tabs)/scan-time-out.tsx
+++ app/(tabs)/scan-time-out.tsx
@@ -13,13 +13,13 @@ export default function ScanTimeOut() {
       end={{ x: 0, y: 1 }}
       className='flex-1'
     >
-      <View className='flex-1 justify-center items-center'>
+      <View className='items-center py-72'>
         <Image
           source={require('../../assets/images/nfc-wayfinding-mark.png')}
           className='w-[250px] h-[200px]'
           resizeMode='contain'
         />
-        <Text className='font-opensans text-[30px] color-[#1e1e1e] mt-[30px]'>Tap Card to Time In.</Text>
+        <Text className='font-opensans text-[30px] color-[#1e1e1e] mt-[30px]'>Tap Card to Time Out.</Text>
         <Pressable
         onPress={() => router.push('/(tabs)/time-out')}
         className='mt-[30px] border border-[#1e1e1e] rounded-[50px] items-center justify-center w-[150px] h-[50px]'
