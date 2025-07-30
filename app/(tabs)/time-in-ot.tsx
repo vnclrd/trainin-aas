2b298c89194f56a08623e21cddb3b8263bd93e30
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-07-30 18:46:19 +0800

    Add animation to time-in-ot

--- app/(tabs)/time-in-ot.tsx
+++ app/(tabs)/time-in-ot.tsx
@@ -1,10 +1,40 @@
-import { View, Text, Pressable } from 'react-native'
+import { View, Text, Pressable, Animated, Easing } from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient'
 import { useRouter } from 'expo-router'
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { useRef, useEffect } from 'react'
 
 export default function TimeInOnTime() {
   const router = useRouter()
+  const translateY = useRef(new Animated.Value(0)).current
+  const opacity = useRef(new Animated.Value(0)).current
+  const opacityFadeAnim = useRef(new Animated.Value(0)).current
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.delay(1000),
+      Animated.timing(translateY, { toValue: 0, duration: 1000, easing: Easing.out(Easing.exp), useNativeDriver: true })
+    ]).start()
+  }, [])
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.delay(1000),
+      Animated.timing(opacity, { toValue: 1, duration: 1000, easing: Easing.out(Easing.exp), useNativeDriver: true })
+    ]).start()
+  }, [])
+
+  useEffect(() => {
+    Animated.sequence([
+      Animated.delay(1000),
+      Animated.loop(
+        Animated.sequence([
+          Animated.timing(opacityFadeAnim, { toValue: 1, duration: 1000, useNativeDriver: true }),
+          Animated.timing(opacityFadeAnim, { toValue: 0, duration: 1000, useNativeDriver: true })
+        ])
+      )
+    ]).start()
+  }, [opacityFadeAnim])
 
   return (
     <LinearGradient
@@ -13,23 +43,31 @@ export default function TimeInOnTime() {
       end={{ x: 0, y: 1 }}
       style={{ flex: 1 }}
     >
-      <SafeAreaView className='flex-1 justify-center items-center'>
+      <SafeAreaView className='flex-1 items-center justify-center'>
+
         <View className='flex-row'>
-          <Text className='font-opensans text-5xl' style={{ lineHeight: 70 }}>Hello,</Text>
-          <Text className='font-opensans-bold text-5xl' style={{ lineHeight: 70 }}> Miguel.</Text>
+          <Animated.Text style={{ transform: [{ translateY }], alignItems: 'center' }}>
+            <Text className='font-opensans text-5xl' style={{ lineHeight: 70 }}>Hello,</Text>
+            <Text className='font-opensans-bold text-5xl' style={{ lineHeight: 70 }}> Miguel.</Text>
+          </Animated.Text>
         </View>
-        <Text>Your time in is:</Text>
-        <Text
-          className='font-opensans-bolditalic text-[70px] color-[#fff]'
-        >
-          7:45 AM
-        </Text>
-        <Pressable
-          onPress={() => router.push('/')}
-          className='absolute bottom-20'
-        >
+        
+        <Animated.Text style={{ opacity: opacity, alignItems: 'center' }}>
+          <Text>Your time in is:</Text>
+        </Animated.Text>
+        <Animated.Text style={{ opacity: opacity, alignItems: 'center' }}>
+          <Text className='font-opensans-bolditalic text-[70px] color-[#fff]'>7:45 AM</Text>
+        </Animated.Text>
+        
+        <Animated.View style={{ opacity: opacityFadeAnim, alignItems: 'center', position: 'absolute', bottom: 80 }}>
+          <Pressable onPress={() => router.push('/')}>
           <Text className='font-opensans text-xl'>Tap to continue</Text>
         </Pressable>
+        </Animated.View>
+        
+          
+        
+        
       </SafeAreaView>
     </LinearGradient>
   )
